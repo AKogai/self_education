f82b67aa6bc7bed3c6a69b1cc92b22b1069c8629
AKogai <aleksejkogaj@yande.ru>
2020-06-16 11:20:33 +0300

    making good looking repo

--- python_mail-mfti/week1/week1(solution).py
+++
@@ -1,8 +0,0 @@
-import sys
-
-
-a = sys.argv[1]
-c = 0
-for d in range(len(a)):
-    c += int(a[d])
-print(c)
--- python_mail-mfti/week1/week1(stairs).py
+++
@@ -1,6 +0,0 @@
-import sys
-
-
-a = int(sys.argv[1])
-for d in range(1, a + 1):
-    print(' ' * (a - d) + '#' * d)
